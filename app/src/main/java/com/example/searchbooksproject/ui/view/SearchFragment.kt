da84c0bcd083e115f53c89a6e7943c47055b9908
Ojongseok <whdtjr6889@naver.com>
2023-04-26 06:49:02 +0900

    [Practice8C] - 네트워크 로딩 상태 핸들링(LoadState, LoadStateAdapter)

--- app/src/main/java/com/example/searchbooksproject/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/searchbooksproject/ui/view/SearchFragment.kt
@@ -6,13 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.searchbooksproject.data.model.Book
 import com.example.searchbooksproject.databinding.FragmentSearchBinding
 import com.example.searchbooksproject.ui.adapter.BookSearchAdapter
+import com.example.searchbooksproject.ui.adapter.BookSearchLoadStateAdapter
 import com.example.searchbooksproject.ui.adapter.BookSearchPagingAdapter
 import com.example.searchbooksproject.ui.viewmodel.BookSearchViewModel
 import com.example.searchbooksproject.util.collectLatestStateFlow
@@ -36,6 +40,7 @@ class SearchFragment: Fragment() {
 
         setupRecyclerView()
         searchBooks()
+        setupLoadState()
 
 //        viewModel.searchResult.observe(viewLifecycleOwner) {
 //            books = it.documents
@@ -53,7 +58,10 @@ class SearchFragment: Fragment() {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
             addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
-            adapter = bookSearchAdapter
+//            adapter = bookSearchAdapter
+            adapter = bookSearchAdapter.withLoadStateFooter(
+                BookSearchLoadStateAdapter(bookSearchAdapter::retry)
+            )
         }
 
         bookSearchAdapter.setItemClickListener(object : BookSearchPagingAdapter.OnItemClickListener {
@@ -84,6 +92,32 @@ class SearchFragment: Fragment() {
             }
             startTime = endTime
         }
+    }
+
+    private fun setupLoadState() {
+        bookSearchAdapter.addLoadStateListener { combinedLoadStates ->
+            val loadState = combinedLoadStates.source
+            val isListEmpty = bookSearchAdapter.itemCount < 1
+                    && loadState.refresh is LoadState.NotLoading
+                    && loadState.append.endOfPaginationReached
+
+            binding.tvEmptylist.isVisible = isListEmpty
+            binding.rvSearchResult.isVisible = !isListEmpty
+            binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
+
+//            binding.btnRetry.isVisible = loadState.refresh is LoadState.Error
+//                    || loadState.append is LoadState.Error
+//                    || loadState.prepend is LoadState.Error
+//            val errorState: LoadState.Error? = loadState.append as? LoadState.Error
+//                ?: loadState.prepend as? LoadState.Error
+//                ?: loadState.refresh as? LoadState.Error
+//            errorState?.let {
+//                Toast.makeText(requireContext(), it.error.message, Toast.LENGTH_SHORT).show()
+//            }
+        }
+//        binding.btnRetry.setOnClickListener {
+//            bookSearchAdapter.retry()
+//        }
     }
 
     override fun onDestroy() {
